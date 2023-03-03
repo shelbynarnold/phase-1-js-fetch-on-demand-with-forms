8cf4051f8aaa0d5a3c5d17fef62275300d20f069
shelby <shelbynarnold@yahoo.com>
2023-03-02 19:43:12 -0700

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const init = () => {
+    const inputForm = document.querySelector("form");
   
-}
+    inputForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const input = document.querySelector("input#searchByID");
+  
+      fetch(`http://localhost:3000/movies/${input.value}`)
+        .then((response) => response.json())
+        .then((data) => {
+          const title = document.querySelector("section#movieDetails h4");
+          const summary = document.querySelector("section#movieDetails p");
+  
+          title.innerText = data.title;
+          summary.innerText = data.summary;
+        });
+    });
+  };
+  
+  document.addEventListener("DOMContentLoaded", init);
 
-document.addEventListener('DOMContentLoaded', init);
+// Add event listeners to capture form data and override a form's default behavior
+// Fetch data based on what the user types into that form
+// // Display that data on the page
